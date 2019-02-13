d2f4d425be103428e1fc355719ffcfb922210221
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-02-13 18:14:40 +0100

    Allow to automatically regenerate containers.

--- projects/money/src/index.js
+++ projects/money/src/index.js
@@ -1,14 +1,10 @@
 import store from './store';
 import registerWebComponent from './lib/register-web-component';
+import WEB_COMPONENTS from './web-components';
+const COMPONENT_PREFIX = 'my-service';
 
-
-registerWebComponent({ store, name: 'my-service--user-sign-up', importComponent: () => import('./container/user-sign-up') });
-registerWebComponent({ store, name: 'my-service--user-sign-in', importComponent: () => import('./container/user-sign-in')} );
-registerWebComponent({ store, name: 'my-service--money-transaction-create', importComponent: () => import('./container/money-transaction-create')} );
-registerWebComponent({ store, name: 'my-service--money-transaction-list', importComponent: () => import('./container/money-transaction-list')} );
-registerWebComponent({ store, name: 'my-service--money-transaction-reports', importComponent: () => import('./container/money-transaction-reports')} );
-registerWebComponent({ store, name: 'my-service--money-transaction-filter', importComponent: () => import('./container/money-transaction-filter')} );
-registerWebComponent({ store, name: 'my-service--user-update', importComponent: () => import('./container/user-update')} );
-registerWebComponent({ store, name: 'my-service--navigation', importComponent: () => import('./container/navigation')} );
-registerWebComponent({ store, name: 'my-service--when-user-authenticated', importComponent: () => import('./container/when-user-authenticated')} );
-
+Object.keys(WEB_COMPONENTS).forEach((key) => {
+	const name = `${COMPONENT_PREFIX}--${key}`;
+	registerWebComponent({ store, name, importComponent: WEB_COMPONENTS[key] });
+	console.info(`Registered: ${name}`);
+});
