5ae172b8613b8a6e744d7417e56aa4ecc9d43262
Thomas Mayrhofer <thomas.mayrhofer@trigo.at>
2019-02-14 15:16:43 +0100

    Remove unused migrations.

--- projects/user/migrations/V3__create_money_transactions.sql
+++
@@ -1,8 +0,0 @@
-create table if not exists
-public.money_transactions (
-  id		   SERIAL PRIMARY KEY,
-  start   timestamp without time zone NOT NULL,
-  until   timestamp without time zone NOT NULL,
-
-  CHECK		(debitor_id <> creditor_id)
-);
