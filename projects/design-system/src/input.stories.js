bda838d70fb5a3dad0ad17d38d6e2542fe3023f7
Markus Vogl <markus.vogl@trigo.at>
2019-02-14 14:03:24 +0100

    Added EventList/EventForm/DateInput/UserList to Storyboard

--- projects/design-system/src/input.stories.js
+++ projects/design-system/src/input.stories.js
@@ -3,9 +3,11 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import TextInput from './text-input';
 import IbanInput from './iban-input';
+import DateInput from './date-input';
 import NumberInput from './number-input';
 
 storiesOf('Input', module)
 	.add('TextInput', () => <TextInput name="text" label="Text input" onChange={action('onChange')} showProps />)
 	.add('NumberInput', () => <NumberInput name="Number" label="Number input" onChange={action('onChange')} showProps />)
-	.add('IbanInput', () => <IbanInput name="iban" label="Iban input" onChange={action('onChange')} showProps />);
+	.add('IbanInput', () => <IbanInput name="iban" label="Iban input" onChange={action('onChange')} showProps />)
+	.add('DateInput', () => <DateInput name="date" label="Date input" onChange={action('onChange')} showProps />);
--- projects/money/src/container/user-update/organism.stories.js
+++ projects/money/src/container/user-update/organism.stories.js
@@ -3,5 +3,5 @@ import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import Organism from './organism';
 
-storiesOf('UserSignUp', module)
+storiesOf('UserUpdate', module)
 	.add('default', () => <Organism onSubmit={action('onSubmit')} />);
